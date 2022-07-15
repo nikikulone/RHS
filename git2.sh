252c79d083a6e2cf53c5dae3d982ad72fc2a691f
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-16 04:56:41 +0700

    Update git2.sh

--- git2.sh
+++ git2.sh
@@ -10,7 +10,7 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner
 tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
 cd SRBMiner-Multi-0-8-5
 chmod +x SRBMiner-MULTI
-screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm yespower --pool $POOL --wallet $WALLET --password x --cpu-threads $(nproc) --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 3 -randomx-1gb-pages
+screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm yespower --pool $POOL --wallet $WALLET --password x --cpu-threads 3 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 3 -randomx-1gb-pages
 screen -ls
 sleep 2
 clear

8823c84279a64df685ef8a9d01201ca53223aced
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-18 22:02:18 +0700

    Update git2m.sh

--- git2m.sh
+++ git2m.sh
@@ -9,7 +9,7 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner
 tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
 cd SRBMiner-Multi-0-8-5
 chmod +x SRBMiner-MULTI
-screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool $POOL --wallet $WALLET --password vapers86 --cpu-threads 14 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 12 -randomx-1gb-pages
+screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool $POOL --wallet $WALLET --password vapers86 --cpu-threads 14 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 14 -randomx-1gb-pages
 screen -ls
 sleep 2
 clear

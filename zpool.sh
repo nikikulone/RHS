99bd55afee6c6b04fbbe0b74807c4123b8f4901f
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-19 11:31:24 +0700

    Update zpool.sh

--- zpool.sh
+++ zpool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WALLET=MWyhsXGGKoUrQ3E7GRn7CZV4UvHX34ePrt.DENT-$(echo $(shuf -i 1-9999 -n 1))
+WALLET=MWyhsXGGKoUrQ3E7GRn7CZV4UvHX34ePrt
 
 POOL=stratum+tcp://minotaurx.sea.mine.zpool.ca:7019
 
@@ -9,7 +9,7 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner
 tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
 cd SRBMiner-Multi-0-8-5
 chmod +x SRBMiner-MULTI
-screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool $POOL --wallet $WALLET --password c=LTC,zap=MAZA --cpu-threads 14 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 14 -randomx-1gb-pages
+screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool $POOL --wallet $WALLET --password c=LTC,zap=AVN/LCC/MAZA --cpu-threads 14 --msr-use-tweaks 0 --msr-use-preset 0 --cpu-threads-intensity 1 --cpu-threads-priority 14 -randomx-1gb-pages
 screen -ls
 sleep 2
 clear

